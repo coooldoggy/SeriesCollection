5e6c8a531648b621239562b00e3dba7e98c6f959
yulimlee <coooldoggy@gmail.com>
2021-05-31 19:24:35 +0900

    - title show hide

--- Project/app/src/main/java/com/coooldoggy/seriescollection/ui/view/SeriesDetailViewFragment.kt
+++ Project/app/src/main/java/com/coooldoggy/seriescollection/ui/view/SeriesDetailViewFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.coooldoggy.seriescollection.R
 import com.coooldoggy.seriescollection.databinding.FragmentDetailViewBinding
 import com.coooldoggy.seriescollection.model.data.Series
 import com.coooldoggy.seriescollection.ui.viewmodel.EpisodeListViewModel
@@ -33,17 +35,37 @@ class SeriesDetailViewFragment: Fragment() {
         arguments?.let {
             viewModel.seriesItem.value = it.getSerializable(EpisodeListViewModel.KEY_SERIES_ITEM) as? Series
         }
+        dataBinding.imgBtnBack.setOnClickListener {
+            view.findNavController().popBackStack()
+        }
+        setResources()
+    }
 
+    private fun setResources(){
         dataBinding.rvEpisode.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = viewModel.adapter
         }
 
-        dataBinding.imgBtnBack.setOnClickListener {
-            view.findNavController().popBackStack()
-        }
-
         dataBinding.layoutHeaderSeries.ivSeriesCover.clipToOutline = true
+        dataBinding.mlDetail.setTransitionListener(object : MotionLayout.TransitionListener{
+            override fun onTransitionStarted(p0: MotionLayout?, startId: Int, endId: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, currentId: Int) {
+                if (currentId == R.id.start){
+                    dataBinding.tvTitle.visibility = View.INVISIBLE
+                }else{
+                    dataBinding.tvTitle.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {
+            }
+        })
 
         viewModel.seriesItem.observe(viewLifecycleOwner, Observer {
             viewModel.getSeriesItem(it.id)
